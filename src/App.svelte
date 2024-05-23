cae8ef2430ff99734db35e5374324987fdb1ecb4
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-05-23 10:13:24 -0500

    navbar como componente

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+import { onMount } from 'svelte';
+import Sidebar from './Sidebar.svelte';
 
-// Funciones JavaScript
-function toggleRootClass() {
+const toggleRootClass = () => {
   const current = document.documentElement.getAttribute('data-bs-theme');
   const inverted = current === 'dark' ? 'light' : 'dark';
   document.documentElement.setAttribute('data-bs-theme', inverted);
 }
 
-function toggleLocalStorage() {
+const toggleLocalStorage = () => {
   if (isLight()) {
     localStorage.removeItem('light');
   } else {
@@ -16,11 +16,10 @@ function toggleLocalStorage() {
   }
 }
 
-function isLight() {
+const isLight = () => {
   return localStorage.getItem('light');
 }
 
-// Montar los event listeners cuando el componente se monta
 onMount(() => {
   const sidebarToggle = document.querySelector('#sidebar-toggle');
   if (sidebarToggle) {
@@ -43,97 +42,12 @@ onMount(() => {
 });
 </script>
 
+<style>
+
+</style>
+
 <div class="wrapper">
-  <aside id="sidebar" class="js-sidebar">
-      <!-- Content For Sidebar -->
-      <div class="h-100">
-          <div class="sidebar-logo">
-              <a href="#">CodzSword</a>
-          </div>
-          <ul class="sidebar-nav">
-              <li class="sidebar-header">
-                  Admin Elements
-              </li>
-              <li class="sidebar-item">
-                  <a href="#" class="sidebar-link">
-                      <i class="fa-solid fa-list pe-2"></i>
-                      Dashboard
-                  </a>
-              </li>
-              <li class="sidebar-item">
-                  <a href="#" class="sidebar-link collapsed" data-bs-target="#pages" data-bs-toggle="collapse"
-                      aria-expanded="false"><i class="fa-solid fa-file-lines pe-2"></i>
-                      Pages
-                  </a>
-                  <ul id="pages" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Page 1</a>
-                      </li>
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Page 2</a>
-                      </li>
-                  </ul>
-              </li>
-              <li class="sidebar-item">
-                  <a href="#" class="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse"
-                      aria-expanded="false"><i class="fa-solid fa-sliders pe-2"></i>
-                      Posts
-                  </a>
-                  <ul id="posts" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Post 1</a>
-                      </li>
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Post 2</a>
-                      </li>
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Post 3</a>
-                      </li>
-                  </ul>
-              </li>
-              <li class="sidebar-item">
-                  <a href="#" class="sidebar-link collapsed" data-bs-target="#auth" data-bs-toggle="collapse"
-                      aria-expanded="false"><i class="fa-regular fa-user pe-2"></i>
-                      Auth
-                  </a>
-                  <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Login</a>
-                      </li>
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Register</a>
-                      </li>
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link">Forgot Password</a>
-                      </li>
-                  </ul>
-              </li>
-              <li class="sidebar-header">
-                  Multi Level Menu
-              </li>
-              <li class="sidebar-item">
-                  <a href="#" class="sidebar-link collapsed" data-bs-target="#multi" data-bs-toggle="collapse"
-                      aria-expanded="false"><i class="fa-solid fa-share-nodes pe-2"></i>
-                      Multi Dropdown
-                  </a>
-                  <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                      <li class="sidebar-item">
-                          <a href="#" class="sidebar-link collapsed" data-bs-target="#level-1"
-                              data-bs-toggle="collapse" aria-expanded="false">Level 1</a>
-                          <ul id="level-1" class="sidebar-dropdown list-unstyled collapse">
-                              <li class="sidebar-item">
-                                  <a href="#" class="sidebar-link">Level 1.1</a>
-                              </li>
-                              <li class="sidebar-item">
-                                  <a href="#" class="sidebar-link">Level 1.2</a>
-                              </li>
-                          </ul>
-                      </li>
-                  </ul>
-              </li>
-          </ul>
-      </div>
-  </aside>
+  <Sidebar />
   <div class="main">
       <nav class="navbar navbar-expand px-3 border-bottom">
           <button class="btn" id="sidebar-toggle" type="button">
@@ -284,6 +198,3 @@ onMount(() => {
   </div>
 </div>
 
-<style>
-
-</style>
--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -1,160 +1,99 @@
 <!-- Sidebar.svelte -->
 <script>
-  (function () {
-  'use strict'
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
+
 </script>
 
 <style>
-body {
-  min-height: 100vh;
-  min-height: -webkit-fill-available;
-}
 
-html {
-  height: -webkit-fill-available;
-}
-
-.b-example-divider {
-  flex-shrink: 0;
-  width: 1.5rem;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, .1);
-  border: solid rgba(0, 0, 0, .15);
-  border-width: 1px 0;
-  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-}
-
-.bi {
-  vertical-align: -.125em;
-  pointer-events: none;
-  fill: currentColor;
-}
-
-.dropdown-toggle { outline: 0; }
-
-.nav-flush .nav-link {
-  border-radius: 0;
-}
-
-.btn-toggle {
-  display: inline-flex;
-  align-items: center;
-  padding: .25rem .5rem;
-  font-weight: 600;
-  color: rgba(0, 0, 0, .65);
-  background-color: transparent;
-  border: 0;
-}
-.btn-toggle:hover,
-.btn-toggle:focus {
-  color: rgba(0, 0, 0, .85);
-  background-color: #d2f4ea;
-}
-
-.btn-toggle::before {
-  width: 1.25em;
-  line-height: 0;
-  content: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='rgba%280,0,0,.5%29' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 14l6-6-6-6'/%3e%3c/svg%3e");
-  transition: transform .35s ease;
-  transform-origin: .5em 50%;
-}
-
-.btn-toggle[aria-expanded="true"] {
-  color: rgba(0, 0, 0, .85);
-}
-.btn-toggle[aria-expanded="true"]::before {
-  transform: rotate(90deg);
-}
-
-.btn-toggle-nav a {
-  display: inline-flex;
-  padding: .1875rem .5rem;
-  margin-top: .125rem;
-  margin-left: 1.25rem;
-  text-decoration: none;
-}
-.btn-toggle-nav a:hover,
-.btn-toggle-nav a:focus {
-  background-color: #d2f4ea;
-}
-
-.scrollarea {
-  overflow-y: auto;
-}
-
-.fw-semibold { font-weight: 600; }
-.lh-tight { line-height: 1.25; }
-
-.bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
 </style>
 
-<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
-  <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-    <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-    <span class="fs-4">Sidebar</span>
-  </a>
-  <hr>
-  <ul class="nav nav-pills flex-column mb-auto">
-    <li class="nav-item">
-      <a href="#" class="nav-link active" aria-current="page">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
-        Home
+<aside id="sidebar" class="js-sidebar">
+  <!-- Content For Sidebar -->
+  <div class="h-100">
+    <div class="sidebar-logo">
+      <a href="#">CodzSword</a>
+    </div>
+    <ul class="sidebar-nav">
+      <li class="sidebar-header">
+        Admin Elements
+      </li>
+      <li class="sidebar-item">
+        <a href="#" class="sidebar-link">
+          <i class="fa-solid fa-list pe-2"></i>
+            Dashboard
+        </a>
+      </li>
+      <li class="sidebar-item">
+      <a href="#" class="sidebar-link collapsed" data-bs-target="#pages" data-bs-toggle="collapse"
+          aria-expanded="false"><i class="fa-solid fa-file-lines pe-2"></i>
+          Pages
       </a>
+      <ul id="pages" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+        <li class="sidebar-item">
+          <a href="#" class="sidebar-link">Page 1</a>
+        </li>
+        <li class="sidebar-item">
+          <a href="#" class="sidebar-link">Page 2</a>
+        </li>
+      </ul>
     </li>
-    <li>
-      <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
-        Dashboard
-      </a>
-    </li>
-    <li>
-      <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
-        Orders
-      </a>
-    </li>
-    <li>
-      <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
-        Products
-      </a>
-    </li>
-    <li>
-      <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
-        Customers
-      </a>
-    </li>
-  </ul>
-  <hr>
-  <div class="dropdown">
-    <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-      <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-      <strong>mdo</strong>
-    </a>
-    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-      <li><a class="dropdown-item" href="#">New project...</a></li>
-      <li><a class="dropdown-item" href="#">Settings</a></li>
-      <li><a class="dropdown-item" href="#">Profile</a></li>
-      <li><hr class="dropdown-divider"></li>
-      <li><a class="dropdown-item" href="#">Sign out</a></li>
+    <li class="sidebar-item">
+        <a href="#" class="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse"
+            aria-expanded="false"><i class="fa-solid fa-sliders pe-2"></i>
+            Posts
+        </a>
+        <ul id="posts" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">Post 1</a>
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">Post 2</a>
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">Post 3</a>
+          </li>
+        </ul>
+      </li>
+      <li class="sidebar-item">
+        <a href="#" class="sidebar-link collapsed" data-bs-target="#auth" data-bs-toggle="collapse"
+            aria-expanded="false"><i class="fa-regular fa-user pe-2"></i>
+            Auth
+        </a>
+        <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">Login</a>
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">Register</a>
+          </li>
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link">Forgot Password</a>
+          </li>
+        </ul>
+      </li>
+      <li class="sidebar-header">
+        Multi Level Menu
+      </li>
+      <li class="sidebar-item">
+        <a href="#" class="sidebar-link collapsed" data-bs-target="#multi" data-bs-toggle="collapse"
+            aria-expanded="false"><i class="fa-solid fa-share-nodes pe-2"></i>
+            Multi Dropdown
+        </a>
+        <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+          <li class="sidebar-item">
+            <a href="#" class="sidebar-link collapsed" data-bs-target="#level-1"
+                data-bs-toggle="collapse" aria-expanded="false">Level 1</a>
+            <ul id="level-1" class="sidebar-dropdown list-unstyled collapse">
+              <li class="sidebar-item">
+                  <a href="#" class="sidebar-link">Level 1.1</a>
+              </li>
+              <li class="sidebar-item">
+                  <a href="#" class="sidebar-link">Level 1.2</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
     </ul>
   </div>
-</div>
+</aside>
